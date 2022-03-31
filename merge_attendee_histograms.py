422f3e2d426b44aadb30f3fd95bcbb52d88db154
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2022-03-31 11:41:18 -0700

    Make `merge_attendee_histogram` expect counts in the last column of the CSV inputs instead of the second column.

--- merge_attendee_histograms.py
+++ merge_attendee_histograms.py
@@ -11,25 +11,25 @@ parser = argparse.ArgumentParser(
   description='Combine attendee histograms.'
 )
 parser.add_argument(
-  'input', metavar='N', type=str, nargs='+',
-  help='A CSV file containing data in the format "name,count".'
+  'input', metavar='input', type=str, nargs='+',
+  help='A CSV file containing data in the format "...,count".'
 )
 args = parser.parse_args()
 
-data = {}
+data = {} # Schema: {attendee : [count0, count1, ..., countN]}
 
-for file in range(len(args.input)):
-  with open(args.input[file]) as csvfile:
-    reader = csv.reader(csvfile)
-    for attendee in csv.reader(csvfile):
-      assert(len(attendee) == 2)
-      if attendee[0] in data:
-        data[attendee[0]][file] = int(attendee[1])
+for fileidx in range(len(args.input)):
+  with open(args.input[fileidx]) as file:
+    for entry in csv.reader(file):
+      attendee = tuple(entry[:-1])
+      count = entry[-1]
+      if attendee in data:
+        data[attendee][fileidx] = int(count)
       else:
-        new_attendee = [0] * len(args.input)
-        new_attendee[file] = int(attendee[1])
-        data[attendee[0]] = new_attendee
+        new_record = [0] * len(args.input)
+        new_record[fileidx] = int(count)
+        data[attendee] = new_record
 
-for attendee in sorted(data.items(), key=lambda x: (sum(x[1]), x[1], x[0]), reverse=True):
-  print("{0},{1}".format(attendee[0], ','.join(map(str, attendee[1]))))
+for record in sorted(data.items(), key=lambda x: (sum(x[1]), x[1], x[0]), reverse=True):
+  print("{0},{1}".format(','.join(record[0]), ','.join(map(str, record[1]))))
 
