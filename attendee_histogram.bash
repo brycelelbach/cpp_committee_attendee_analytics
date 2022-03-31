305423684771c7e520e092e753ef7edcb55ecf08
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2022-03-31 11:36:29 -0700

    Run `split_attendee_field.py` in `attendee_histogram.bash`.

--- attendee_histogram.bash
+++ attendee_histogram.bash
@@ -4,4 +4,16 @@
 #
 # Distributed under the Boost Software License v1.0 (boost.org/LICENSE_1_0.txt)
 
-cat ${@} | dos2unix | sed 's/^[ ]*[*-] //' | sort | uniq -c | sed 's/^[ \t]*//' | sed 's/[ ]/,/' | awk -F ',' '{ printf "%s,%s\n", $2, $1 }' | sort -k2rn -t,
+SCRIPT_PATH=$(cd $(dirname ${0}); pwd -P)
+
+cat ${@}                                      \
+  | dos2unix                                  \
+  | sed 's/^[ ]*[*-] //'                      \
+  | sort                                      \
+  | uniq -c                                   \
+  | sed 's/^[ \t]*//'                         \
+  | sed 's/[ ]/,/'                            \
+  | awk -F ',' '{ printf "%s,%s\n", $2, $1 }' \
+  | sort -k2rn -t,                            \
+  | ${SCRIPT_PATH}/split_attendee_field.py
+
